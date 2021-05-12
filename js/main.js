656d7b6260d97af79aa17f41224f13a8e871f4f8
LidiyaZzz <Lida.ngs@mail.ru>
2021-05-12 11:50:06 +0700

    create: map location settings

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
 
@@ -19,6 +19,6 @@ const swiper = new Swiper('.swiper-container', {
     enabled: true,
     onlyInViewport: true,
     pageUpDown: true,
-  }
+  },
 
 });
